7ef90aa4ca82d893a22fc9cd4a5d4a027c6de256
Cristian Paolini <cristianpaolini3@gmail.com>
2021-01-26 13:54:36 -0300

    Mejora en la subida de archivos y configuración de cabeceras HTTP y acceso CORS implementada

--- backend/controllers/project.js
+++ backend/controllers/project.js
@@ -1,6 +1,7 @@
 'use strict'
 
 var Project = require('../models/project');
+var fs = require('fs');
 
 var controller = {
 
@@ -109,19 +110,28 @@ var controller = {
 			var filePath = req.files.image.path;
 			var fileSplit = filePath.split('\\');
 			var fileName = fileSplit[1];
+			var extSplit = fileName.split('\.');
+			var fileExt = extSplit[1];
 
+			if (fileExt == 'png' || fileExt == 'jpg' || fileExt == 'jpeg' || fileExt == 'gif') {
+				Project.findByIdAndUpdate(projectId, {image: fileName}, {new: true}, (err, projectUpdated) => {
+					if (err) return res.status(200).send({message: 'La imagen no se ha subido.'});
 
-			Project.findByIdAndUpdate(projectId, {image: fileName}, {new: true}, (err, projectUpdated) => {
-				if (err) return res.status(200).send({message: 'La imagen no se ha subido.'});
+					if (!projectUpdated) return res.status(404).send({message: 'El proyecto no existe y no se ha asignado la imagen.'});
 
-				if (!projectUpdated) return res.status(404).send({message: 'El proyecto no existe y no se ha asignado la imagen.'});
-
-				return res.status(200).send({
+					return res.status(200).send({
 					project: projectUpdated
 				});
 
-
 			});
+		}else{
+
+			fs.unlink(filePath, (err) => {
+				return res.status(200).send({message: 'La extensión no es válida'});
+			});
+
+		}
+
 
 		}else{
 			return res.status(200).send({
